a6ed55e1f8d4102d533e61ef1dc7a5d9fa384b27
usylvie12 <uwasylvie12@outlook.com>
2022-09-19 15:10:26 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/*.html"],
+  theme: {
+    extend: {
+      spacing: {
+        '112': '28rem',
+        '128': '32rem',
+      }
+    },
+  },
+  plugins: [],
+}
